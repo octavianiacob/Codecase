e5c23cae21de19e816c0512ed94b1d2f73d3f0eb
Octavian Iacob <octavian.iacob@icloud.com>
2021-03-25 16:06:59 +0200

    Added React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Homepage from './containers/Homepage';
+import Explore from './containers/Explore';
+import Dashboard from './containers/Dashboard';
 
 const App = () => {
   return (
-    <h1 className='font-bold'>Hello World!</h1>
+		<Router>
+			<Switch>
+				<Route path='/' exact>
+					<Homepage />
+				</Route>
+				<Route path='/explore' exact>
+					<Explore />
+				</Route>
+				<Route path='/dashboard' exact>
+					<Dashboard />
+				</Route>
+				<Redirect to='/' />
+			</Switch>
+		</Router>
   );
 }
 
---
+++ frontend/src/containers/Explore.js
@@ -0,0 +1,7 @@
+const Explore = () => {
+    return ( 
+        <h1>Explore page</h1>
+     );
+}
+ 
+export default Explore;
