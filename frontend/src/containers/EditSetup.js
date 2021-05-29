d730d3774c3216013fced7d0a75149e11752a949
Octavian Iacob <octavian.iacob@icloud.com>
2021-05-29 14:46:17 +0300

    Sort Setups on Explore

--- frontend/src/containers/EditSetup.js
+++ frontend/src/containers/EditSetup.js
@@ -23,11 +23,11 @@ const EditSetup = () => {
         const req = await axios.get(`/api/setups/${setupID}`);
         setPrevData(req.data.setup);
         setToolsList(req.data.setup.tools);
-        setToolsListIDs(req.data.setup.tools.map(t => t._id));
+        setToolsListIDs(req.data.setup.tools.map((t) => t._id));
       } catch (err) {
         console.log(err);
       }
-    }
+    };
     fetchPrevData();
   }, [setupID]);
 
@@ -39,165 +39,191 @@ const EditSetup = () => {
       } catch (err) {
         console.log(err);
       }
-    }
+    };
     fetchTools();
   }, []);
 
   let options = [];
 
   if (tools.length > 0) {
-    options = tools.map((tool) => ({ value: tool._id, label: tool.title, category: tool.category, description: tool.description }));
+    options = tools.map((tool) => ({
+      value: tool._id,
+      label: tool.title,
+      category: tool.category,
+      description: tool.description
+    }));
   }
 
   const handleChange = (selectedOption) => {
     setSelectedOption(selectedOption);
-  }
+  };
 
   const addTool = () => {
     if (selectedOption !== null && !toolsList.includes(selectedOption)) {
-      setToolsList(prevTools => [...prevTools, selectedOption]);
+      setToolsList((prevTools) => [...prevTools, selectedOption]);
     }
-  }
+  };
 
   const removeTool = (index) => {
-    setToolsList(toolsList.filter(item => item !== toolsList[index]));
-  }
-  
+    setToolsList(toolsList.filter((item) => item !== toolsList[index]));
+  };
+
   let initialValues = {
     title: prevData?.title,
     description: prevData.description,
     creator: user._id,
     tools: toolsListIDs,
-    notes: prevData.notes,
-  }
+    notes: prevData.notes
+  };
   return (
     <>
       <Formik
         enableReinitialize
         initialValues={initialValues}
         onSubmit={(values) => {
-          values.tools = toolsList.map((tool => tool.value || tool.id));
+          values.tools = toolsList.map((tool) => tool.value || tool.id);
           axios.patch(`/api/setups/${setupID}`, values);
           dispatch(fetchUser());
           window.location.href = '/dashboard';
-        }}
-      >
-        <Form className="max-w-6xl px-5 m-10 mx-auto space-y-8 divide-y divide-gray-200">
-          <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+        }}>
+        <Form className='max-w-6xl px-5 m-10 mx-auto space-y-8 divide-y divide-gray-200'>
+          <div className='space-y-8 divide-y divide-gray-200 sm:space-y-5'>
             <div>
               <div>
-                <h3 className="text-lg font-medium leading-6 text-gray-900">Basic Information</h3>
-                <p className="max-w-2xl mt-1 text-sm text-gray-500">
-                  Provide a short introduction for your setup
-                </p>
+                <h3 className='text-lg font-medium leading-6 text-gray-900'>Basic Information</h3>
+                <p className='max-w-2xl mt-1 text-sm text-gray-500'>Provide a short introduction for your setup</p>
               </div>
-              <div className="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
-                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                  <label className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                    Setup title
-                  </label>
-                  <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <div className='mt-6 space-y-6 sm:mt-5 sm:space-y-5'>
+                <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <label className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'>Setup title</label>
+                  <div className='mt-1 sm:mt-0 sm:col-span-2'>
                     <Field
-                      type="text"
-                      name="title"
-                      id="title"
-                      className="block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm"
+                      type='text'
+                      name='title'
+                      id='title'
+                      className='block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm'
                     />
                   </div>
                 </div>
-                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                  <label className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                    Description
-                </label>
-                  <div className="mt-1 sm:mt-0 sm:col-span-2">
-                    <Field as='textarea'
-                      id="description"
-                      name="description"
+                <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <label className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'>Description</label>
+                  <div className='mt-1 sm:mt-0 sm:col-span-2'>
+                    <Field
+                      as='textarea'
+                      id='description'
+                      name='description'
                       placeholder={`${prevData.description}`}
                       rows={3}
-                      className="block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className='block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                     />
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="pt-8 divide-y divide-gray-200 sm:pt-10">
+            <div className='pt-8 divide-y divide-gray-200 sm:pt-10'>
               <div>
-                <h3 className="text-lg font-medium leading-6 text-gray-900">Tools and Technologies</h3>
-                <p className="max-w-2xl mt-1 text-sm text-gray-500">Add all apps, tools, programming languages, frameworks and libraries you use in development</p>
+                <h3 className='text-lg font-medium leading-6 text-gray-900'>Tools and Technologies</h3>
+                <p className='max-w-2xl mt-1 text-sm text-gray-500'>
+                  Add all apps, tools, programming languages, frameworks and libraries you use in development
+                </p>
               </div>
               <FieldArray name='notes'>
                 {({ remove }) => (
-                  <div className="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
-                    <div className="pt-3 sm:border-t sm:border-gray-200 sm:pt-5">
-                      {toolsList.length < 1 ? null :
-                        toolsList.map((tool, index) => (
-                          <div key={tool.value || tool._id} className="my-5 overflow-hidden bg-white shadow sm:rounded-lg">
-                            <div className="flex justify-between px-4 py-5 sm:px-6">
-                              <h3 className="text-lg font-medium leading-6 text-gray-900">{tool.label || tool.title}</h3>
-                              <button
-                                type='button'
-                                onClick={() => {
-                                  remove(index);
-                                  removeTool(index);
-                                }}
-                                className="text-gray-500 hover:text-red-500"
-                              >
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                              </button>
+                  <div className='mt-6 space-y-6 sm:mt-5 sm:space-y-5'>
+                    <div className='pt-3 sm:border-t sm:border-gray-200 sm:pt-5'>
+                      {toolsList.length < 1
+                        ? null
+                        : toolsList.map((tool, index) => (
+                            <div
+                              key={tool.value || tool._id}
+                              className='my-5 overflow-hidden bg-white shadow sm:rounded-lg'>
+                              <div className='flex justify-between px-4 py-5 sm:px-6'>
+                                <h3 className='text-lg font-medium leading-6 text-gray-900'>
+                                  {tool.label || tool.title}
+                                </h3>
+                                <button
+                                  type='button'
+                                  onClick={() => {
+                                    remove(index);
+                                    removeTool(index);
+                                  }}
+                                  className='text-gray-500 hover:text-red-500'>
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='w-6 h-6'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    stroke='currentColor'>
+                                    <path
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                      strokeWidth={2}
+                                      d='M6 18L18 6M6 6l12 12'
+                                    />
+                                  </svg>
+                                </button>
+                              </div>
+                              <div className='border-t border-gray-200'>
+                                <dl>
+                                  <div className='px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                                    <dt className='text-sm font-medium text-gray-500'>Description</dt>
+                                    <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                                      {tool.description}
+                                    </dd>
+                                  </div>
+                                  <div className='px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                                    <dt className='text-sm font-medium text-gray-500'>Category</dt>
+                                    <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                                      {tool.category}
+                                    </dd>
+                                  </div>
+                                  <div className='px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                                    <label className='text-sm font-medium text-gray-500'>Notes</label>
+                                    <Field
+                                      as='textarea'
+                                      id={`note-${tool.value}`}
+                                      name={`notes[${index}]`}
+                                      rows={3}
+                                      placeholder={prevData.notes[index]}
+                                      className='block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                                    />
+                                  </div>
+                                </dl>
+                              </div>
                             </div>
-                            <div className="border-t border-gray-200">
-                              <dl>
-                                <div className="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                  <dt className="text-sm font-medium text-gray-500">Description</dt>
-                                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{tool.description}</dd>
-                                </div>
-                                <div className="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                  <dt className="text-sm font-medium text-gray-500">Category</dt>
-                                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{tool.category}</dd>
-                                </div>
-                                <div className="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                  <label className="text-sm font-medium text-gray-500">Notes</label>
-                                  <Field as='textarea'
-                                    id={`note-${tool.value}`}
-                                    name={`notes[${index}]`}
-                                    rows={3}
-                                    placeholder={prevData.notes[index]}
-                                    className="block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                  />
-                                </div>
-                              </dl>
-                            </div>
-                          </div>
-                        ))
-                      }
+                          ))}
                     </div>
                   </div>
                 )}
               </FieldArray>
 
-              <div className="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
-                <div className="pt-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                  <label htmlFor="title" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              <div className='mt-6 space-y-6 sm:mt-5 sm:space-y-5'>
+                <div className='pt-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+                  <label htmlFor='title' className='block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2'>
                     Add a tool or technology
                   </label>
-                  <div className="flex items-center mt-3 sm:col-span-2">
+                  <div className='flex items-center mt-3 sm:col-span-2'>
                     <Select
                       onChange={handleChange}
                       options={options}
-                      className="block w-2/3 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className='block w-2/3 pl-3 pr-10 text-base border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                     />
-                    <span className="ml-3">
+                    <span className='ml-3'>
                       <button
                         onClick={addTool}
-                        type="button"
-                        className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-1 -ml-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                          <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+                        type='button'
+                        className='inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500'>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          className='w-5 h-5 mr-1 -ml-2 text-gray-400'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'>
+                          <path
+                            fillRule='evenodd'
+                            d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z'
+                            clipRule='evenodd'
+                          />
                         </svg>
                         <span>Add</span>
                       </button>
@@ -207,18 +233,16 @@ const EditSetup = () => {
               </div>
             </div>
           </div>
-          <div className="pt-5">
-            <div className="flex justify-end">
+          <div className='pt-5'>
+            <div className='flex justify-end'>
               <Link
                 to='/dashboard'
-                className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
+                className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                 Cancel
               </Link>
               <button
-                type="submit"
-                className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
+                type='submit'
+                className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                 Save
               </button>
             </div>
@@ -226,7 +250,7 @@ const EditSetup = () => {
         </Form>
       </Formik>
     </>
-  )
-}
+  );
+};
 
 export default EditSetup;
--- frontend/src/containers/Explore.js
+++ frontend/src/containers/Explore.js
@@ -2,16 +2,18 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SetupsList from '../components/SetupsList';
-import Filters from '../components/Filters';
 import Pagination from '../components/Pagination';
 import Spinner from '../components/Spinner';
+import Card from '../components/Card';
+import Dropdown from '../components/Dropdown';
 
 const Explore = () => {
-
   const [setups, setSetups] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [setupsPerPage] = useState(9);
+  const [sortedSetups, setSortedSetups] = useState([]);
+  const [sortType, setSortType] = useState('updatedAt');
 
   useEffect(() => {
     const fetchSetups = async () => {
@@ -20,24 +22,73 @@ const Explore = () => {
       const setups = req.data.setups;
       setSetups(setups);
       setLoading(false);
-    }
+    };
     fetchSetups();
   }, []);
+
   const indexOfLastSetup = currentPage * setupsPerPage;
   const indexOfFirstSetup = indexOfLastSetup - setupsPerPage;
   const currentSetups = setups.slice(indexOfFirstSetup, indexOfLastSetup);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  useEffect(() => {
+    const sortArray = type => {
+      const types = {
+        updatedAt: 'updatedAt',
+        likes: 'likes'
+      };
+      const sortProperty = types[type];
+      let sorted = [];
+      if(type === 'updatedAt') {
+        sorted = [...currentSetups].sort((a, b) => {
+          return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
+        });
+      }
+      else if(type === 'likes') {
+        sorted = [...currentSetups].sort((a, b) => {
+          return b.likes - a.likes;
+        });
+      }
+      setSortedSetups(sorted);
+    };
+
+    sortArray(sortType);
+  }, [sortType, setups]);
+
   return (
     <>
-      {!setups ? <Spinner/> :
+      {!setups ? (
+        <Spinner />
+      ) : (
         <>
-          <Filters items={setups} />
-          <SetupsList setups={currentSetups} loading={loading} />
-          <Pagination setupsPerPage={setupsPerPage} totalSetups={setups.length} currentPage={currentPage} paginate={paginate} />
+          <Card className='m-3 sm:m-0'>
+            <div className='px-4 py-5 bg-white border-b border-gray-200 sm:px-6'>
+              <div className='flex flex-col items-center justify-between -mt-4 -ml-4 sm:flex-row sm:items-center sm:flex-nowrap'>
+                <h1 className='text-xl font-semibold sm:text-3xl'>All Setups</h1>
+                <div className='flex flex-col mt-5 sm:justify-center sm:flex-row sm:mt-0'>
+                  <Dropdown
+                    className='mx-6 mt-5 sm:mt-0'
+                    label='Sort by'
+                    id='date'
+                    name='date'
+                    options={[{text: 'Last Updated', value: 'updatedAt'}, {text: 'Most likes', value: 'likes'}]}
+                    onChange={(e) => setSortType(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </Card>
+          <SetupsList setups={sortedSetups} loading={loading} />
+          <Pagination
+            setupsPerPage={setupsPerPage}
+            totalSetups={setups.length}
+            currentPage={currentPage}
+            paginate={paginate}
+          />
         </>
-      }
+      )}
     </>
   );
-}
+};
 
 export default Explore;
